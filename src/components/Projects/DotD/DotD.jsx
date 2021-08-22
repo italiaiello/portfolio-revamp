47a614be0132d3b326d6d7e031e7d81fe2c3605b
Andrew Aiello <andrewaiello7@gmail.com>
2021-08-22 10:26:30 +1000

    Added rest of project images to the site

---
+++ src/components/Projects/DotD/DotD.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import DotdImage from '../../../assets/images/dotd.JPG'
+import D3Icon from '../../../assets/icons/technical-icons/d3.svg'
+import GitHubIcon from '../../../assets/icons/technical-icons/github.svg'
+import ReactIcon from '../../../assets/icons/technical-icons/react.svg'
+
+
+const DotD = ({ largePulse, smallPulse, showLargeOrSmall }) => {
+
+    return (
+        <section className="project center-column">
+            <article className="project__summary center-row">
+                {
+                    showLargeOrSmall === 'large' ?
+                    <figure className="project__summary--large-pulse">
+                        <img src={largePulse} className="responsive-img" alt="Large pulse"/>
+                    </figure>
+                    :
+                    <figure className="project__summary--small-pulse">
+                        <img src={smallPulse} className="responsive-img" alt="Small pulse"/>
+                    </figure>
+                }
+                <article className="project__summary--fig-container center-column">
+                    <figure className="project__summary--fig-container__fig">
+                        <img src={DotdImage} alt="Day of the Dead virtual reality experience" className="responsive-img" />
+                    </figure>
+                    <article className="project__summary--fig-container__technical-logos">
+                        <figure className="project__summary--fig-container__technical-logos--logo">
+                            <img src={D3Icon} alt="D3" className="responsive-img" />
+                        </figure>
+                        <figure className="project__summary--fig-container__technical-logos--logo">
+                            <img src={GitHubIcon} alt="GitHub" className="responsive-img" />
+                        </figure>
+                        <figure className="project__summary--fig-container__technical-logos--logo">
+                            <img src={ReactIcon} alt="React" className="responsive-img" />
+                        </figure>
+                    </article>
+                </article>
+                <article className="project__summary--desc">
+                    <h3 className="project__summary--desc__title">Day of the Dead VR</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat ex ac gravida blandit. Nunc quis neque purus. Morbi 
+                        tincidunt magna laoreet, porta magna non, ultrices justo. Donec id dui massa. Nam blandit vestibulum vehicula. Sed et neque 
+                        nec ipsum pellentesque interdum quis sollicitudin tortor. Donec porta et erat nec faucibus. Nullam auctor, urna non malesuada 
+                        consequat, augue urna egestas massa, ut porttitor lorem risus et justo. Maecenas sit amet nunc at ante convallis dictum eu sed 
+                        sapien. Phasellus magna neque, varius non sollicitudin et, rutrum vel quam.
+                    </p>
+                    <div className="project__summary--desc__btn-container center-row">
+                        <button className="btn">Try It Out</button>
+                        <button className="btn">See Code on GitHub</button>
+                    </div>
+                </article>
+            </article>
+        </section>
+    )
+}
+
+export default DotD
--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import MassShootingsUS from '../Projects/MassShootingsUS/MassShootingsUS'
 import DiabetesAssist from '../Projects/DiabetesAssist/DiabetesAssist'
+import Nervi from '../Projects/Nervi/Nervi'
+import FootballRoundUp from '../Projects/FootballRoundUp/FootballRoundUp'
+import DotD from '../Projects/DotD/DotD'
+import SmartBrain from '../Projects/SmartBrain/SmartBrain'
 import LargePulse from '../../assets/pulses/large-pulse.png'
 import SmallPulse from '../../assets/pulses/small-pulse.png'
 
@@ -10,9 +14,12 @@ const ProjectsSection = () => {
     return (
         <section className="projects-section">
             <h2 className="section-heading">Projects Section</h2>
-            <DiabetesAssist largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'large'} />
+            <Nervi largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'large'} />
             <MassShootingsUS largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'small'} />
             <DiabetesAssist largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'large'} />
+            <SmartBrain largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'small'} />
+            <DotD largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'large'} />
+            <FootballRoundUp largePulse={LargePulse} smallPulse={SmallPulse} showLargeOrSmall={'small'} />
         </section>
     )
 }
